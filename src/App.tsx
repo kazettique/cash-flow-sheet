4ef776811a70963eea4df5e406c8c7229386d2d1
Woody <kazettique@gmail.com>
2022-02-17 22:56:46 +0800

    Env Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+import Hello from '@/components/Hello';
 
 function App() {
   return (
@@ -10,14 +11,10 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React 5566
         </a>
+        <Hello />
       </header>
     </div>
   );
